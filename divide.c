3f802eadb2d22e871bb4862c1fe71e10546615f7
Yihua Pu <puyihua1@gmail.com>
2018-09-05 00:47:31 -0400

    now can tackle negative numbers

--- divide.c
+++ divide.c
@@ -1,20 +1,45 @@
 #include "types.h"
 #include "user.h"
 
+int str2int(char str_x[]){
+	int x;
+	if (str_x[0] == '-') 
+		x = - atoi(&(str_x[1]));
+	else
+		x = atoi(str_x);
+	return x;
+}
+
 
 
 int main(int argc, char *argv[]){
     if (argc != 3) {
-        printf(1,"divide: usage divide x y \n");}
-    int x = atoi(argv[1]);
-    int y = atoi(argv[2]);
+        printf(1,"divide: usage divide x y \n");
+		exit();
+	}
+    int x = str2int(argv[1]);
+    int y = str2int(argv[2]);
+	if (y==0){
+		printf(1,"divide: cannot divide by ZERO!!\n");
+		exit();
+	}
+
+	int nega;
+	nega = 0;
     int re1, re2 ,re3;  //error happended when define float variable
-    re1 = x/y;
+	if (x*y<0)
+		nega = 1;
+	x = x<0? -x:x;
+	y = y<0? -y:y;
+	re1 = x/y;
     re2 = (10*x/y)%10;
     re3 = (100*x/y)%10;
     
 
-    printf(1, "%s / %s is %d", argv[1],argv[2],re1);
+    printf(1, "%s / %s is ", argv[1],argv[2]);
+	if (nega)
+		printf(1,"-");
+	printf(1,"%d",re1);
     if ((re2==0)&&(re3==0))
         printf(1,"\n");
     else if (re3==0)
