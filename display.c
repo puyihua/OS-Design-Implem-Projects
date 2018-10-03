9994a0d4d5603ddcf1509d6aa8304e134424ceea
Yihua Pu <puyihua1@gmail.com>
2018-10-03 00:04:41 -0500

    fully finish part2

--- display.c
+++ display.c
@@ -13,16 +13,51 @@
 #include "vga.h"
 #include "memlayout.h"
 
+#define CRTPORT 0x3d4
+static ushort *crt = (ushort*)P2V(0xb8000);
+ushort contentBuf[2000];
+//2000 = 80*25
 static struct {
   struct spinlock lock;
   int locking;
 } disp;
 unsigned int pixel = 0;
+int curPosition = 0;
 
+void getCurPosition()
+{
+	outb(CRTPORT,14);
+	curPosition = inb(CRTPORT+1)<<8;
+	outb(CRTPORT,15);
+	curPosition |= inb(CRTPORT+1);
+}
+
+void setCurPosition()
+{
+	outb(CRTPORT,14);
+	outb(CRTPORT+1,curPosition>>8);
+	outb(CRTPORT,15);
+	outb(CRTPORT+1,curPosition);
+	crt[curPosition] = ' '| 0x0700;
+}
+void getContent()
+{ 
+	int i;
+	for(i=0;i<curPosition;i++)
+		contentBuf[i]=crt[i];
+}
+void setContent()
+{
+	int i;
+	for(i=0;i<2000;i++)
+		crt[i] = 0;
+	for(i=0;i<curPosition;i++)
+		crt[i]=contentBuf[i];
+}
 int
 displayread(struct file *f, char *buf, int n)
 {
-  return 1;
+  return -1;
 }
 
 int
@@ -33,9 +68,6 @@ displaywrite(struct file *f, char *buf, int n)
   for(i=0;i<n;i++)
   {
     vid[i+1000*pixel] = buf[i];
-    //vid[i] = 0xCC;
-
-
   }
   pixel++;
   return n;
@@ -53,10 +85,19 @@ int displayioctl(struct file * f,int param,int value)
 {
   switch(param){
     case 1:
-      if(value==0x3)
-        vgaMode3();
       if(value==0x13)
+	  {
+	    getCurPosition();
+		getContent();
         vgaMode13();
+	  }
+      if(value==0x3)
+	  {
+		vgaMode3();
+		setCurPosition();
+		setContent();
+
+	  }
 	  break;
 
 	case 2:
@@ -67,8 +108,6 @@ int displayioctl(struct file * f,int param,int value)
 	  int b = value&0xff;
 	  vgaSetPalette(index,r,g,b);
 	  break;
-
-
   }
   return 1;
 }
